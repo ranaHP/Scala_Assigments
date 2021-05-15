d9e9b17d77422eb07d5350c9ee7c4500e426cf42
ranaHP <2019cs135@gmail.com>
2021-05-15 22:46:24 +0530

    Scala Assigment 001 updated

--- questions.scala
+++ questions.scala
@@ -5,8 +5,8 @@ object answers extends App{
 
   def whole_sale_price(n:Int) = if(n>50) 24.95* 60 / 100 * n + (n - 50 ) * 0.75 +50*3 else 24.95*60/100 * n + n*3;
 
-  println("Celsius 35 is equal to : " + celsius_to_fahrenheit(35) + "f");
-  println("volume of radius 5 sphere is : " + Volume_of_sphere(5));
+  println("Celsius 35 is equal  : " + celsius_to_fahrenheit(35) + "f");
+  println("volume of radius 5 sphere - : " + Volume_of_sphere(5));
   println("Whole scale of books :" + whole_sale_price(60));
 
 }
